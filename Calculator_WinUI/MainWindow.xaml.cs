ab908b9cfbfc2c411ced3794876820f1e17cd47a
Daniel Čech <daniel.j.cech@gmail.com>
2026-06-08 19:15:09 +0200

    refactor: added custom title bar and added new app icon

--- Calculator_WinUI/MainWindow.xaml.cs
+++ Calculator_WinUI/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-using Calculator_WinUI.Views; 
+using Calculator_WinUI.Views;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Windowing;
+using WinUIEx;
 
 namespace Calculator_WinUI
 {
@@ -9,9 +11,28 @@ namespace Calculator_WinUI
         public MainWindow()
         {
             this.InitializeComponent();
+            this.AppWindow.SetIcon("Assets\\Icon\\Icon.ico");
 
             MainFrame.Navigate(typeof(StandardPage));
             NavView.SelectedItem = NavView.MenuItems[0];
+
+            // AppWindow configuration
+            // theme 
+            AppWindow.TitleBar.PreferredTheme = TitleBarTheme.UseDefaultAppMode;
+            AppWindow.TitleBar.ExtendsContentIntoTitleBar = true;
+
+            if (AppWindow.TitleBar.ExtendsContentIntoTitleBar)
+            {
+                AppWindow.TitleBar.PreferredHeightOption = TitleBarHeightOption.Standard;
+                AppWindow.TitleBar.ButtonBackgroundColor = Microsoft.UI.Colors.Transparent;
+                AppWindow.TitleBar.ButtonInactiveBackgroundColor = Microsoft.UI.Colors.Transparent;
+
+            }
+            // size
+            this.SetWindowSize(330, 480);
+            var manager = WinUIEx.WindowManager.Get(this);
+            manager.MinWidth = 300;
+            manager.MinHeight = 450;
         }
 
         private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
