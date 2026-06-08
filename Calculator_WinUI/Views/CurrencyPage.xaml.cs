0d981cc750e04645c206f639bff62394b45c263b
Daniel Čech <daniel.j.cech@gmail.com>
2026-06-08 19:14:02 +0200

    refactor: adjusted the currency selection flyout

--- Calculator_WinUI/Views/CurrencyPage.xaml.cs
+++ Calculator_WinUI/Views/CurrencyPage.xaml.cs
@@ -13,5 +13,16 @@ namespace Calculator_WinUI.Views
             this.InitializeComponent();
             ViewModel = new CurrencyViewModel();
         }
+
+
+        // hide flyouts when a currency is selected
+        private void ListView1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            FlyoutCurrency1.Hide();
+        }
+        private void ListView2_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            FlyoutCurrency2.Hide();
+        }
     }
 }
