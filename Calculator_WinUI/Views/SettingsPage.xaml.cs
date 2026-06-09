c28f39709e783f1eac45e02f9d0756435affe43d
Daniel Čech <daniel.j.cech@gmail.com>
2026-06-09 03:36:23 +0200

    feat: created first draft of a settings page

---
+++ Calculator_WinUI/Views/SettingsPage.xaml.cs
@@ -0,0 +1,46 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+namespace Calculator_WinUI.Views
+{
+    public sealed partial class SettingsPage : Page
+    {
+        public SettingsPage()
+        {
+            InitializeComponent();
+        }
+
+        // theme combo box
+        private void ThemeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (sender is ComboBox comboBox && comboBox.SelectedItem is ComboBoxItem selectedItem)
+            {
+                string themeTag = selectedItem.Tag?.ToString();
+
+                // we get the absolute root element of the current window
+                if (this.XamlRoot?.Content is FrameworkElement rootElement)
+                {
+                    // Match-Mapping for the ElementTheme enum
+                    rootElement.RequestedTheme = themeTag switch
+                    {
+                        "Light" => ElementTheme.Light,
+                        "Dark" => ElementTheme.Dark,
+                        _ => ElementTheme.Default // system default
+                    };
+                }
+            }
+        }
+    }
+}
